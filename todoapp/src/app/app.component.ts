6911835f4b15d8a456320a0ebe05fa7ad1b49c27
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-10-23 21:37:46 -0300

    componenetes creados y routing también

---
+++ todoapp/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule,RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'todoapp';
+  welcome = 'Hola,Facu';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear el proyeecto',
+    'Crear componentes'
+  ]
+}
---
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.scss'
+})
+export class LabsComponent {
+  welcome = 'Hola,Facu';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear el proyeecto',
+    'Crear componentes'
+  ]
+}
