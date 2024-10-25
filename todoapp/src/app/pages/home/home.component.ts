5621fa43af3a1fd7f7fa711d7bc2a65883059c9e
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-10-25 18:15:57 -0300

    add and delete task done.

--- todoapp/src/app/pages/home/home.component.ts
+++ todoapp/src/app/pages/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,signal,Signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  tasks = signal( [
+    'Instalar Angular CLI',
+    'Crear el proyeecto',
+    'Crear componentes',
+    'Trabajar con signals es bueno',
+    'Armar presupuestos',
+    'Armar una estratégia de Marketing'
+  ])
 
+  //agregando una tarea
+  changeHandler(event:Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks,newTask]);
+    input.value = '';
+  }
+  deleteTask(index: Number){
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index )); //filtro dejando por fuera el que coincide en la posición.
+  }
 }
