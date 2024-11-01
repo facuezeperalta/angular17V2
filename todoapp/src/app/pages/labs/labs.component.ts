746ce5f631ce75546b0bfce7dbdf1b60dbf03240
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-11-01 16:17:38 -0300

    cambiar el nombre segun cantidad de items completado.

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -22,11 +22,11 @@ export class LabsComponent {
   image = "https://http2.mlstatic.com/D_NQ_NP_940349-MLA78300890977_082024-O.webp";
   AltImage1 = "Esta es una imagen de una cámara Z6III";
   //creo un obejto persona.
-  person ={
-    name: "Facundo",
+  person =signal({
+    name: "facundo",
     age: 29,
     avatar: "https://i.pinimg.com/736x/c0/74/9b/c0749b7cc401421662ae901ec8f9f660.jpg"
-  }
+  })
   clickhandler(){
     alert("Botón pulsado")
   }
